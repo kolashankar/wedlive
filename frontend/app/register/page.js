30bd0663fee08493da2c749acf07723aa0c51383
kolashankar <kolashankar113@gmail.com>
2025-12-06 20:49:21 +0530

    frontend env changes

--- frontend/app/register/page.js
+++ frontend/app/register/page.js
@@ -64,7 +64,7 @@ export default function RegisterPage() {
                 <Input
                   id="fullName"
                   type="text"
-                  placeholder="John Doe"
+                  placeholder="Shiva"
                   value={fullName}
                   onChange={(e) => setFullName(e.target.value)}
                   required
